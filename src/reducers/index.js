4e2cdeff91fbc63706c70269ea3ebc9f42f52838
redelization <ricardosmithco@gmail.com>
2019-10-16 11:36:29 -0500

    created actions and reducers; next ill wire them up

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+    return [
+        {title: 'No Woman No Cry', Artist: 'Bob Marley', duration: '4:05'},
+        {title: 'Toast', Artist: 'Koffee', duration: '3:25'},
+        {title: 'No weapon', Artist: 'Fred Hammond', duration: '3:45'}
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if(action.type === 'SONG_SELECTED'){
+        return action.payload;
+    }
+
+    return selectedSong;
+};
